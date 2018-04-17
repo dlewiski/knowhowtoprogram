100b3a79ddccc7b2cdb10f2b34e6f10f04a84f2c
david lewis and roger yee <roger@>
2018-04-17 14:12:02 -0700

    add show view for lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,11 @@
 class LessonsController < ApplicationController
   def index
     @lessons = Lesson.all
-    render :index
+    @section = Section.find(params[:section_id])
+  end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+    @section = Section.find(params[:section_id])
   end
 end
